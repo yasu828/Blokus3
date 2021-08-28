203c2e32810ff94a1f2076df45db9b75c02bdfd7
yasu828 <69378134+yasu828@users.noreply.github.com>
2021-08-28 19:24:36 +0900

    vue add routerコマンド

--- src/App.vue
+++ src/App.vue
@@ -1,184 +1,32 @@
 <template>
   <div id="app">
-    <BlokusHeader class="header"></BlokusHeader>
-    <LeftPlayersBlock1 
-    class="player1"
-    @click="handClick"
-    >
-    </LeftPlayersBlock1>
-
-    <LeftPlayersBlock2 
-    class="player2"
-    @click="handClick"
-    >
-    </LeftPlayersBlock2>
-
-    <!-- ブロックを配置する基盤 -->
-    <div class="table">
-      <table>
-        <tr 
-          v-for="(boardRow, y) in mainBoard" 
-          :key="y"
-        >
-          <td 
-            class="MainTableCell" 
-            group="items" 
-            v-for="(cell, x) in boardRow" 
-            :key="x" 
-            :class="`is-${cell.code}`" 
-            @dragover.prevent
-            @dragenter.prevent
-            @drop="droped(x, y)"
-          >
-          </td>
-        </tr>
-      </table>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
     </div>
-    <!-- //ブロックを配置する基盤 -->
-
-    <RightPlayersBlock1 
-    class="player3"
-    @click="handClick"
-    >
-    </RightPlayersBlock1>
-    
-    <RightPlayersBlock2 
-    class="player4"
-    @click="handClick"
-    >
-    </RightPlayersBlock2>
+    <router-view/>
   </div>
 </template>
 
-<script>
-
-import RightPlayersBlock1 from './components/RightPlayersBlock1.vue'
-import RightPlayersBlock2 from './components/RightPlayersBlock2.vue'
-import LeftPlayersBlock1 from './components/LeftPlayersBlock1.vue'
-import LeftPlayersBlock2 from './components/LeftPlayersBlock2.vue'
-import BlokusHeader from './components/BlokusHeader.vue'
-export default {
-  components: {
-    BlokusHeader,
-    RightPlayersBlock1,
-    RightPlayersBlock2,
-    LeftPlayersBlock1,
-    LeftPlayersBlock2,
-  },
-  name: 'App',
-  data (){
-    const mainBoard = [];
-      for (let y = 0; y < 20; y++ ) {
-        const boardRow = []
-        for (let x = 0; x < 20; x++){
-          const cell = {
-            y: y,
-            x: x,
-            code: 0,
-          };
-          boardRow.push(cell)
-        }
-        mainBoard.push(boardRow)
-      } 
-// ここからvue.js記述
-    return{
-      mainBoard: mainBoard,
-      item: [],
-    };
-  },
-  methods:{
-    handClick(chooseBlock){
-      this.item = chooseBlock
-    },
-    droped(x, y){
-      for (let i = 0; i < 5; i++) {
-        for (let j = 0; j < 5; j++) {
-          if (this.mainBoard[y + i][x + j].code < 1) {
-            this.mainBoard[y + i][x + j].code = this.item[i][j].code;
-          }
-        }
-      }
-      this.item3 = []
-    },
-  }
-}
-</script>
-
-<style scoped lang="scss">
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-	background-color: rgba(123, 113, 102, 0.6);
-	background-image:radial-gradient(#5a5e59 1px, transparent 1px);
-	background-size: 20px 20px;
-  display: grid;
-  height: 100vh;
-  grid-template: 
-  "top  top  top  top  top  top  top"  12%
-  ".... .... .... .... .... .... ...." 3%
-  ".... p1   main main main p3   ...." 42%
-  ".... .... main main main ...  ...." 1%
-  ".... p2   main main main p4   ...." 42%
-  /10px 1fr  2fr  2fr  2fr  1fr  10px;
-}
-// grid-areaここから
-.header{
-  grid-area: top;
-}
-.table{
-  grid-area: main;
-  background-color: white;
-  height: 91%;
-  border: black solid 3px;
-  margin: auto;
-}
-.player1{
-  grid-area: p1;
-  display: flex;
-  margin: auto;
-}
-.player2{
-  grid-area: p2;
-  display: flex;
-  margin: auto;
-}
-.player3{
-  grid-area: p3;
-  display: flex;
-  margin: auto;
-}
-.player4{
-  grid-area: p4;
-  display: flex;
-  margin: auto;
-}
-// grid-areaここまで
-
-.MainTableCell{
-  height: 20px;
-  width: 20px;
-  border: solid black 1px;
 }
 
-.is-1{
-    background-color: rgb(10, 241, 29);
-}
-.is-2{
-    background-color: rgb(4, 73, 250);
-}
-.is-3{
-    background-color: rgb(248, 29, 29);
-}
-.is-4{
-    background-color: rgb(244, 181, 10);
-}
+#nav {
+  padding: 30px;
 
-.chooseblocks{
-  height: 120px;
-  width: 120px;
-  background-color: beige;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
